ff797b2e2eef5546d1e9b1787e0f52bce4c1f3e4
Ulan <ulankarimov@mail/ru>
2018-03-03 19:26:32 +0600

    second

--- post/admin.py
+++ post/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
 
+
 from post.models import Category, Food
 
-
 # Register your models here.
+
+from django.contrib.auth.models import Group, User
+
+admin.site.unregister(Group)
+admin.site.unregister(User)
+
+
+class FoodAdmin(admin.StackedInline):
+    model = Food
+    extra = 1
+    readonly_fields = 'created_at updated_at'.split()
 
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -11,17 +22,7 @@ class CategoryAdmin(admin.ModelAdmin):
         model = Category
 
     readonly_fields = 'created_at updated_at'.split()
+    inlines = [FoodAdmin]
 
 
 admin.site.register(Category, CategoryAdmin)
-
-
-
-class FoodAdmin(admin.ModelAdmin):
-    class Meta:
-        model = Food
-
-    readonly_fields = 'created_at updated_at'.split()
-
-
-admin.site.register(Food, FoodAdmin)
--- post/models.py
+++ post/models.py
@@ -14,11 +14,12 @@ class Category(models.Model):
     class Meta:
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
-    position = models.IntegerField(verbose_name="приоритет")
+
+    position = models.IntegerField(verbose_name="приоритет", null=True, blank=True)
     title = models.CharField(max_length=50, verbose_name="Название")
-    title_eng = models.CharField(max_length=50, verbose_name="Название(англ.)")
-    title_tr = models.CharField(max_length=50, verbose_name="Название(турец.)",null=True)
-    title_kg = models.CharField(max_length=50, verbose_name="Название(кырг.)",null=True)
+    title_eng = models.CharField(max_length=50, verbose_name="Название(англ.)", null=True, blank=True)
+    title_tr = models.CharField(max_length=50, verbose_name="Название(турец.)", null=True, blank=True)
+    title_kg = models.CharField(max_length=50, verbose_name="Название(кырг.)", null=True, blank=True)
     img = models.ImageField(upload_to=image_upload_to, verbose_name='Иконка')
     created_at = models.DateTimeField(auto_now_add=True, blank=True)
     updated_at = models.DateTimeField(auto_now=True, blank=True)
@@ -29,21 +30,21 @@ class Category(models.Model):
 
 class Food(models.Model):
     class Meta:
-        verbose_name = 'Меню'
-        verbose_name_plural = 'Меню'
+        verbose_name = 'Добавить блюдо'
+        verbose_name_plural = 'Добавить блюдо'
 
-    dish = models.ForeignKey(Category, verbose_name='категория', null=True)
+    dish = models.ForeignKey(Category, verbose_name='категория', null=True, blank=True)
     img = models.ImageField(upload_to=image_upload_to, verbose_name='Иконка')
     title = models.CharField(max_length=50, verbose_name="Название")
-    title_eng = models.CharField(max_length=50, verbose_name="Название(англ.)")
-    title_tr = models.CharField(max_length=50, verbose_name="Название(турец.)", null=True)
-    title_kg = models.CharField(max_length=50, verbose_name="Название(кырг.)",null=True)
-    description = models.CharField(max_length=500, verbose_name="Описание", null=True)
-    description_eng = models.CharField(max_length=500, verbose_name="Описание(англ.)", null=True)
-    description_tr = models.CharField(max_length=500, verbose_name="Описание(турец.)", null=True)
-    description_kg = models.CharField(max_length=500, verbose_name="Описание(кырг.)", null=True)
-    weight = models.CharField(max_length=100, verbose_name="Вес(Выход)", null=True)
-    price = models.IntegerField(verbose_name="Стоимость", null=True)
+    title_eng = models.CharField(max_length=50, verbose_name="Название(англ.)", null=True, blank=True)
+    title_tr = models.CharField(max_length=50, verbose_name="Название(турец.)", null=True, blank=True)
+    title_kg = models.CharField(max_length=50, verbose_name="Название(кырг.)", null=True, blank=True)
+    description = models.CharField(max_length=500, verbose_name="Описание", null=True, blank=True)
+    description_eng = models.CharField(max_length=500, verbose_name="Описание(англ.)", null=True, blank=True)
+    description_tr = models.CharField(max_length=500, verbose_name="Описание(турец.)", null=True, blank=True)
+    description_kg = models.CharField(max_length=500, verbose_name="Описание(кырг.)", null=True, blank=True)
+    weight = models.CharField(max_length=100, verbose_name="Вес(Выход)", null=True, blank=True)
+    price = models.IntegerField(verbose_name="Стоимость", null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True, blank=True)
     updated_at = models.DateTimeField(auto_now=True, blank=True)
 
